165f31975f86f381af92db85349f69feda0503e6
KhandakarShakilRana <khandakarshakilrana770@gmail.com>
2026-04-01 10:11:29 +0600

    get Started

--- src/components/GetStated.jsx
+++ src/components/GetStated.jsx
@@ -14,7 +14,7 @@ const GetStated = () => {
           Start using premium digital tools in minutes, not hours.
         </p>
       </div>
-      <div className="flex flex-col md:flex-row justify-center items-center gap-10 flex-wrap">
+      <div className="flex justify-center items-center gap-10 flex-wrap">
         <div className="flex flex-col max-w-75 items-center max-w-[300px] h-70  justify-center gap-4 p-6 rounded-4xl shadow-sm relative border-4 border-[#e9edf0]">
           <div className="bg-[#4F39F6] h-10 w-10 rounded-full flex items-center justify-center text-white absolute top-4 right-4 shadow-xl">
             01
